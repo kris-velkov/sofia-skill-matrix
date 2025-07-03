875e562f3075c8bbe82bb5d39618ae2f3bce8568
kristiyan.velkov <kristiyan.velkov@ffw.com>
2025-07-03 16:42:16 +0300

    [feat] add margin on the dashboard cards

--- app/statistics/layout.tsx
+++ app/statistics/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function StatisticsLayout({
   children,
 }: {
-  children: React.ReactNode;
+  readonly children: React.ReactNode;
 }) {
   return <ProtectedAdminRoute>{children}</ProtectedAdminRoute>;
 }
